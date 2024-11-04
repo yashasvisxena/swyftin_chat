b944715a370e8654ef7ed2f83a21dd6c66f0ab23
yashasvisxena <yashasvi1802@gmail.com>
2024-11-04 12:36:59 +0530

    responsive done

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/components/ui/sidebar";
+import { ChevronRight } from "lucide-react";
 import React from "react";
 
 const page = () => {
+  const { isMobile, toggleSidebar } = useSidebar();
   return (
-    <div className="size-full flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-medium">Select a chat to start messaging</h1>
+    <div className="size-full flex flex-col justify-center items-center gap-4">
+      <h1 className="text-4xl font-medium text-center">
+        Select a chat to start messaging
+      </h1>
+      {isMobile && <Button onClick={toggleSidebar}>Select Chat</Button>}
     </div>
   );
 };
